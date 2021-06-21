8283b42d44e43090af2d0d584b0002a4e5703728
BlxyHub <86222461+BlxyHub@users.noreply.github.com>
2021-06-21 09:54:06 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,3 @@
--- loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/BlxyHub/BlxyHub/a8d5d74b6759564412fd0dade4c8d9d1a7aae9ea/Main.lua",true))()
+-- loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/BlxyHub/BlxyHub/main/Main.lua",true))()
 
 warn("BlxyHub loaded!")
